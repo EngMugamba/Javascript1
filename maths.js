6a8dd22ce4fe7a965fc0c105c9d1aa5fe33c055f
EngMugamba <mugambakelvin0@gmail.com>
2023-02-28 20:36:51 +0300

    Random number

---
+++ maths.js
@@ -0,0 +1,19 @@
+var won;
+    var num=parseInt(prompt("Enter any number between 1 and 20 and stand a chance to win 10,000!\n"));
+    var Depo=parseInt(prompt("Enter any amount you wish to deposit\n"));
+    console.log("You choose number:\n",num);
+    console.log("You Deposited:\n",Depo);
+
+
+    var x = console.log("The lucky number is:\n",Math.floor(Math.random() * 5));
+n
+if(num == x)
+{
+   won = Depo * 10;
+  console.log("You won",won);
+}
+else
+{
+    console.log("Ooops!!you have lost the chance, if you may wish try again!!", won);
+}
+
