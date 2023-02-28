87787eb1ae04275a925547896225fea8e1d365f1
EngMugamba <mugambakelvin0@gmail.com>
2023-02-28 20:59:13 +0300

    My random number code

--- maths.js
+++ maths.js
@@ -1,12 +1,11 @@
-var won;
+
     var num=parseInt(prompt("Enter any number between 1 and 20 and stand a chance to win 10,000!\n"));
     var Depo=parseInt(prompt("Enter any amount you wish to deposit\n"));
     console.log("You choose number:\n",num);
     console.log("You Deposited:\n",Depo);
 
 
-    var x = console.log("The lucky number is:\n",Math.floor(Math.random() * 5));
-n
+    var x = Math.floor((Math.random() * 20));
 if(num == x)
 {
    won = Depo * 10;
